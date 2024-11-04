e1ae5d58245014833934586f72db77212217b366
Kevin Baluyot <baluyotkevin33@yahoo.com>
2024-11-03 17:07:42 -0800

    added s3 buckets

--- client/next.config.mjs
+++ client/next.config.mjs
@@ -1,5 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+    images: {
+        remotePatterns: [
+            {
+                protocol: "https",
+                hostname: "kb-pm-s3-images.s3.us-east-1.amazonaws.com",
+                port: "",
+                pathname: "/**",
+            }
+        ]
+    }
 };
 
 export default nextConfig;
