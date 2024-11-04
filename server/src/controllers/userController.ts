815ba6b853ad024afd7cac319dcea3974a23b850
Kevin Baluyot <baluyotkevin33@yahoo.com>
2024-11-04 08:47:19 -0800

    added create user route

--- server/src/controllers/userController.ts
+++ server/src/controllers/userController.ts
@@ -12,3 +12,28 @@ export const getUsers = async (req: Request, res: Response): Promise<void> => {
         res.status(500).json({ message: `Error retrieving users: ${err.message}`});
     }
 }
+
+export const createUser = async (req: Request, res: Response): Promise<void> => {
+    try {
+        const {
+            username,
+            cognitoId,
+            profilePictureUrl = "i1.jpg",
+            teamId = 1,
+        } = req.body
+
+        const newUser = await prisma.user.create({
+            data: {
+                username,
+                cognitoId,
+                profilePictureUrl,
+                teamId,
+            }
+        });
+
+        res.json({ message: "User Created Successfully", newUser })
+
+    } catch (err : any) {
+        res.status(500).json({ message: `Error creating user ${err.message}`});
+    }
+}
